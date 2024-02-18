a8f4bf78d2bbf16a2d1e5438ecc1aa07d0ab8a4c
Sergey Shershikov <ssher250110@yandex.ru>
2024-02-18 23:07:16 +0300

    added company_ids and url

---
+++ settings.py
@@ -0,0 +1,16 @@
+# ids компаний
+company_ids = [
+    "84585",  # avito
+    "882",  # 1C
+    "230005",  # 3Logic Group
+    "999442",  # amoCRM
+    "56551",  # AZIMUT Hotel
+    "9311920",  # DNS Технологии
+    "2488771",  # Directum
+    "3148",  # СИТИЛИНК
+    "1122462",  # Skyeng
+    "12849",  # Филип Моррис Интернэшнл
+]
+
+HH_URL_EMPLOYERS = "https://api.hh.ru/employers/"
+HH_URL_VACANCIES = "https://api.hh.ru/vacancies?employer_id="
